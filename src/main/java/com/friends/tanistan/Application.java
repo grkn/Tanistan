c0a0163071a2eca0dbc29ac6e5fc2c4c4f487b14
Orhan Ayhan Illeez <gurkanilleez@gmail.com>
2019-08-22 18:26:30 +0300

    master ready dont touch

--- src/main/java/com/friends/tanistan/Application.java
+++ src/main/java/com/friends/tanistan/Application.java
@@ -1,41 +1,19 @@
 package com.friends.tanistan;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import java.io.File;
-import java.io.IOException;
-
 @SpringBootApplication
-@PropertySources(value = {@PropertySource(value = "application.properties"),
-        @PropertySource(value = "application-persistent.properties")})
+@PropertySources(value = { @PropertySource(value = "application.properties"),
+		@PropertySource(value = "application-persistent.properties") })
 // @RequiredArgsConstructor
 public class Application {
 
-    private static Process p;
-
-    public static void main(String[] args) throws IOException {
-        //launchChromeDriver();
-        SpringApplication.run(Application.class, args);
-    }
-
-    private static void launchChromeDriver() throws IOException {
-        File f = new File("");
-        System.out.println(f.getAbsolutePath());
-        String[] cmd = {f.getAbsolutePath() + "\\driver\\chromedriver.exe"};
-        p = Runtime.getRuntime().exec(cmd);
-        if(p.isAlive()) {
-            System.out.println("Chrome driver is alive");
-        }
-    }
-
-    @PreDestroy
-    public void destory() {
-        p.destroyForcibly();
-        System.out.println("Chrome driver is destroyed");
-    }
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(Application.class, args);
+	}
 }
